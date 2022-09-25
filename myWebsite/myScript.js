a651d1b9fa28e4f25f06449857149471b7931c11
Diane Grant <98796743+Grant-diane@users.noreply.github.com>
2022-09-24 22:29:21 -0400

    Delete myWebsite directory

--- myWebsite/myScript.js
+++
@@ -1,9 +0,0 @@
-var count = 0;
-function myFunction() {
-  count = count + 1;
-  if (count % 2 == 0) {
-  document.getElementById("demo").innerHTML = "Back to a Paragraph.";
-  } else {
-  document.getElementById("demo").innerHTML = "Paragraph changed.";
-  }
-}
